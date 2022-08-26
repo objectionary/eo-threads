5ed899ef6c0e96088649962373a3b97ff5f08aa2
levBagryansky <28lev11@gmail.com>
2022-08-26 17:13:48 +0300

    join

---
+++ src/main/java/EOorg/EOeolang/EOthreads/EOthread$EOjoin.java
@@ -0,0 +1,85 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2022 Objectionary.com
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * EO org.eolang.threads package.
+ *
+ * @since 0.0
+ * @checkstyle PackageNameCheck (4 lines)
+ */
+package EOorg.EOeolang.EOthreads;
+
+import org.eolang.AtComposite;
+import org.eolang.Data;
+import org.eolang.PhDefault;
+import org.eolang.Phi;
+import org.eolang.XmirObject;
+
+/**
+ * AS_INT.
+ *
+ * @checkstyle TypeNameCheck (5 lines)
+ * @since 0.0
+ */
+@XmirObject(oname = "text.as-int")
+public class EOthread$EOjoin extends PhDefault {
+
+    private Thread java_thread;
+    /**
+     * Ctor.
+     *
+     * @param sigma Sigma
+     */
+    public EOthread$EOjoin(final Phi sigma) {
+        super(sigma);
+        this.add(
+            "φ",
+            new AtComposite(
+                this,
+                rho -> {
+                    final Long result = Long.valueOf(5);
+                    return new Data.ToPhi(result);
+                }
+            )
+        );
+    }
+
+}
+
+class ThreadExecuteObj extends Thread{
+    Phi objToExecute;
+    AtComposite atComposite;
+    ThreadExecuteObj(Phi obj){
+        this.objToExecute = obj;
+    }
+
+    public void run(){
+        this.atComposite = new AtComposite(
+            objToExecute,
+            rho -> {
+                return new Data.ToPhi(null);
+            }
+        );
+    }
+}
